9a313f314c068bc0cb848d7947d2a3867b2a6bab
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-15 15:14:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+string[] cibiPreferiti = new string[5] { "Pizza", "Sushi", "Mortadella", "Nutella", "Tiramisù"};
+Console.WriteLine($"Lunghezza array : {cibiPreferiti.Length}"); // Lunghezza array
+
+for(int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine($"Posizione {i+1} : {cibiPreferiti[i]}"); // Lista cibi
+}
+
+
+Console.WriteLine($"Il mio cibo preferito: {cibiPreferiti[0]}"); // Cibo Top
+
+Console.WriteLine($"Il mio cibo preferito, ma non troppo: {cibiPreferiti[cibiPreferiti.Length-1]}"); // Cibo Top
