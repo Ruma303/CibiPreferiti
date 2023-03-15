d5e334ea99dea92d7d76c8da8a6cca8c100fe61e
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-15 15:21:13 +0100

    Bonus ok

--- Program.cs
+++ Program.cs
@@ -11,3 +11,5 @@ for(int i = 0; i < cibiPreferiti.Length; i++)
 Console.WriteLine($"Il mio cibo preferito: {cibiPreferiti[0]}"); // Cibo Top
 
 Console.WriteLine($"Il mio cibo preferito, ma non troppo: {cibiPreferiti[cibiPreferiti.Length-1]}"); // Cibo meno preferito
+
+Console.WriteLine($"Il mio cibo preferito, ma non troppo: {cibiPreferiti[(cibiPreferiti.Length - 1)/2]}"); // Cibo a centro classifica
